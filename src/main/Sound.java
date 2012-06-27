051dee333d5e4e84ebfd273e5ad36023c9e7d09a
Peet Cremer <Peet.Cremer@gmail.com>
2012-06-27 14:51:54 +0200

    Sound von Bomben repariert

--- src/main/Sound.java
+++ src/main/Sound.java
@@ -5,10 +5,16 @@ import java.applet.AudioClip;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-public class Sound {
+public class Sound implements Runnable {
 
-	public Sound(String URL, boolean loop) {
+	protected Thread thread = null;
+	protected AudioClip sound;
+	protected int duration;
+
+	public Sound(String URL, int duration) {
+		this.duration = duration;
 		File urlFile = new File(URL);
 
 		try {
@@ -16,19 +22,50 @@ public class Sound {
 				throw new FileNotFoundException(urlFile.toString());
 			}
 			URL url = urlFile.toURI().toURL();
-			AudioClip sound = Applet.newAudioClip(url);
-
-			if (!loop) {
-				sound.play();
-
-				// sound.stop();
-			} else {
-				sound.loop();
-			}
-			Thread.sleep(1000);
-
+			sound = Applet.newAudioClip(url);
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
+
+		
 	}
+
+	public synchronized void start() {
+		if (thread == null) {
+			if (duration > 0) {
+				sound.play();
+			} else {
+				sound.loop();
+			}
+			thread = new Thread(this);
+			thread.start();
+		}
+	}
+
+	public synchronized void stop() {
+		if (thread != null) {
+			sound.stop();
+			thread = null;
+		}
+
+	}
+
+	public void run() {
+		while (thread != null) {
+			if (duration > 0) {
+				// Keine Endlosschleife
+
+				sound.play();
+				try {
+					TimeUnit.MILLISECONDS.sleep(duration);
+				} catch (InterruptedException e) {
+				}
+				stop();
+			} else {
+				// Endlosschleife
+			}
+		}
+
+	}
+
 }
