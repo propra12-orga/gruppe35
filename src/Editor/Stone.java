fff71d8aa87eac172a713ef0d398ce7ea9d93713
fawol100 <fabian.wolf@hhu.de>
2012-07-02 19:09:35 +0200

    Editor unabhaengig gemmacht

---
+++ src/Editor/Stone.java
@@ -0,0 +1,35 @@
+package Editor;
+
+import main.Global;
+
+/**
+ * 
+ * The Stone class is a child of the Field class and is simply the type of field
+ * which is solid, and does not change even when bombs explode in its vicinity
+ * <P>
+ * 
+ * @author Peet
+ */
+
+public class Stone extends Field {
+
+	// Kopiere dieses Feld
+	public Stone copy() {
+		return new Stone(this);
+	}
+
+	// Copy Constructor
+	public Stone(Stone stone) {
+		super(stone);
+	}
+
+	// Default Constructor
+	public Stone() {
+		this.solid = true;
+		this.transformto = null;
+		this.image = Global.mauersolid;
+		this.spawnsPowerup = false;
+		this.exit = false;
+		this.fieldtype = "S";
+	}
+}
